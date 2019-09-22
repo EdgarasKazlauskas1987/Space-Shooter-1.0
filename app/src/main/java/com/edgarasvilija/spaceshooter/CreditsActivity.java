62d2c4ebf0f5aed5155c5f8a89208fd30b3db2fe
Edgaras Kazlauskas <edgarasvilija@gmail.com>
2019-09-22 18:14:25 +0200

    Fixed wrong button bug

--- app/src/main/java/com/edgarasvilija/spaceshooter/CreditsActivity.java
+++ app/src/main/java/com/edgarasvilija/spaceshooter/CreditsActivity.java
@@ -1,7 +1,11 @@
 package com.edgarasvilija.spaceshooter;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CreditsActivity extends AppCompatActivity {
 
@@ -9,5 +13,25 @@ public class CreditsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_credits);
+
+        Button backButton = (Button) findViewById(R.id.buttonBack);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        Button playButton = (Button) findViewById(R.id.buttonPlay);
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), GameActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
